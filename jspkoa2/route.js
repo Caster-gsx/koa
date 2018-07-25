f5e1a8342053631181109bd554596ffad138a680
Caster-gsx <944780299@qq.com>
2018-07-25 21:25:42 +0800

    koa原生路由实现

---
+++ jspkoa2/route.js
@@ -0,0 +1,49 @@
+// koa原生路由实现
+const Koa = require('koa');
+const app = new Koa();
+const fs = require('fs');
+
+function render(page) {
+    return new Promise((resolve, reject) => {
+        // let pageUrl = './page/' + page;
+        let pageUrl = `./page/${page}`;
+        fs.readFile(pageUrl,"binary",(err, data) => {
+            if(err) {
+                reject(err);
+            }
+            else {
+                resolve(data);
+            }
+        })
+    })
+}
+
+async function route(url) {
+    let page = 'index.html';
+    switch(url) {
+        case '/':
+        case '/index':
+            page = 'index.html';
+            break;
+        case '/todo':
+            page = 'todo.html';
+            break;
+        case '/404':
+            page = '404.html';
+            break;
+        default:
+            break;
+    }
+    let html = await render(page);
+    return html;
+}
+
+app.use(async (ctx) => {
+    let url = ctx.request.url;
+    let html = await route(url);
+    console.log(html);
+    ctx.body = html;
+})
+
+app.listen(3000);
+console.log('server is running ar port 3000');
